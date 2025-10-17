e7da7604d047b649e3d144e4164484b47aeb7097
MUROKO <muroko@users.noreply.github.com>
2025-10-17 01:37:50 -0400

    add: abis/bridgeFeeContractAbi

---
+++ src/abis/bridgeFeeContractAbi.ts
@@ -0,0 +1,16 @@
+// src/abis/bridgeFeeContractAbi.ts
+export const erc20DecimalsAbi = [
+	{
+		"inputs": [],
+		"name": "feePercent",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+] as const;
