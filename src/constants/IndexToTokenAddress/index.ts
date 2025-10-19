e7eeb83bf0d7131ab72f51380e97daf2e65fab08
MUROKO <muroko@users.noreply.github.com>
2025-10-19 13:44:24 -0400

    edit: changes made in constants

---
+++ src/constants/IndexToTokenAddress/index.ts
@@ -0,0 +1,19 @@
+import {
+  BNB_tokenAddress
+} from '@/constants/BNB';
+import {
+  ETH_tokenAddress
+} from '@/constants/ETH';
+import {
+  BASE_tokenAddress
+} from '@/constants/BASE';
+import {
+  TICS_tokenAddress
+} from '@/constants/TICS';
+
+export const chainIndexToTokenAddress = [
+	ETH_tokenAddress,
+	BASE_tokenAddress,
+	BNB_tokenAddress,
+	TICS_tokenAddress
+];
